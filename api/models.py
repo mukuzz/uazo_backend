c78c26fe133637783b6f59906622e3dd1d623aba
Mukul Singh <mukulsingh1248@gmail.com>
2021-01-25 23:38:45 +0530

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class QcInput(models.Model):
+    pass
+
+
+class ProductionSession(models.Model):
+    pass
+
+
+class Style(models.Model)
+    pass
+
+
+class ProductionOrder(models.Model)
+    pass
