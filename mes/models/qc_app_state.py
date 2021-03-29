63cd123f753b795740de7d3148d44961d782e852
Mukul Singh <mukulsingh1248@gmail.com>
2021-03-30 02:13:00 +0530

    wip

--- mes/models/qc_app_state.py
+++ mes/models/qc_app_state.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 from django.conf import settings
 
 class QcAppState(models.Model):
-    signed_in_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True)
+    signed_in_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     datetime = models.DateTimeField(default=timezone.now)
     inputs_count = models.PositiveIntegerField()
     persisted_inputs_count = models.PositiveIntegerField()
     synced_inputs_count = models.PositiveIntegerField()
-    production_session = models.ForeignKey('mes.ProductionSession', on_delete=models.PROTECT)
+    production_session = models.ForeignKey('mes.ProductionSession', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.datetime} - {self.production_session}'
