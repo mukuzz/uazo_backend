ba445fc4f4a6155e0d724f5648de5dd8e32b6ee2
Mukul Singh <mukulsingh1248@gmail.com>
2021-04-19 00:33:43 +0530

    changed static files dir

--- uazo/settings.py
+++ uazo/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/_.uazo.in/html/b/static'
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+STATIC_URL = '/b/static/'
 
 
 REST_FRAMEWORK = {
